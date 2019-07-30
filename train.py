c6a431e91f5dd452500002c297a4a106fac4f445
Yang Liu <liuyang12@focusmedia.cn>
2019-07-30 15:57:50 +0800

    update

--- train.py
+++ train.py
@@ -104,6 +104,8 @@ def train(train_loader, model, criterion, optimizer, epoch, logger):
     for i, (image, text) in enumerate(train_loader):
         # Move to GPU, if available
         image = image.to(device)
+        print(text)
+        print(type(text))
         text = text.to(device)
         batch_size = image.size(0)
 
