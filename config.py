abe1ca4133dcd28765c631216b1481b6e3377764
Yang Liu <liuyang12@focusmedia.cn>
2019-07-30 17:51:16 +0800

    update

--- config.py
+++ config.py
@@ -21,5 +21,5 @@ annotation_files = {'train': 'mnt/ramdisk/max/90kDICT32px/annotation_train.txt',
 # Training parameters
 num_workers = 4  # for data-loading
 grad_clip = 5.  # clip gradients at an absolute value of
-print_freq = 100  # print training/validation stats  every __ batches
+print_freq = 10  # print training/validation stats  every __ batches
 checkpoint = None  # path to checkpoint, None if none
