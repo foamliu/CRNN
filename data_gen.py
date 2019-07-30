6199ba5315466cf3ddb8a845c8fd872b9bf49b41
Yang Liu <liuyang12@focusmedia.cn>
2019-07-30 16:23:58 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -15,7 +15,7 @@ data_transforms = {
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ]),
-    'test': transforms.Compose([
+    'val': transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
