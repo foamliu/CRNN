52462f32223effb07d9d7ccee02fe29edb8f902b
Yang Liu <liuyang12@focusmedia.cn>
2019-07-30 14:02:01 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -1,5 +1,5 @@
 import os
-
+import numpy as np
 import cv2 as cv
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset
@@ -22,16 +22,17 @@ class MJSynthDataset(Dataset):
         return self.lines
 
     def __getitem__(self, i):
-        line = self.lines[i]
-        img_path = line.split(' ')[0]
-        img_path = os.path.join(IMG_FOLDER, img_path)
-        text = str(img_path.split('_')[1].lower())
-        img = cv.imread(img_path, 0)
-        img = cv.resize(img, (imgW, imgH), cv.INTER_CUBIC)
-        img = self.toTensor(img)
-        img.sub_(0.5).div_(0.5)
-
-        return img#, text
+        # line = self.lines[i]
+        # img_path = line.split(' ')[0]
+        # img_path = os.path.join(IMG_FOLDER, img_path)
+        # text = str(img_path.split('_')[1].lower())
+        # img = cv.imread(img_path, 0)
+        # img = cv.resize(img, (imgW, imgH), cv.INTER_CUBIC)
+        # img = self.toTensor(img)
+        # img.sub_(0.5).div_(0.5)
+        #
+        # return img#, text
+        return np.zeros((3, 224, 224))
 
 
 if __name__ == "__main__":
