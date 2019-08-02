354429c06d8cad15451b287a1bdb9e286e01483f
Yang Liu <liuyang12@focusmedia.cn>
2019-08-02 11:30:01 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -21,6 +21,35 @@ data_transforms = {
 }
 
 
+def image_resize(image, width, height, inter=cv.INTER_AREA):
+    old_size = image.shape[:2]  # old_size is in (height, width) format
+    old_ratio = old_size[0] / old_size[1]
+    new_ratio = height / width
+
+    if old_ratio == new_ratio:
+        im = cv.resize(image, (width, height), inter)
+    elif old_ratio > new_ratio:
+        new_width = int(round(old_size[1] * (height / old_size[0])))
+        im = cv.resize(image, (new_width, height), inter)
+    else:
+        new_height = int(round(old_size[0] * (width / old_size[1])))
+        im = cv.resize(image, (width, new_height), inter)
+
+    # new_size should be in (height, width) format
+    new_size = im.shape[:2]
+
+    delta_w = width - new_size[1]
+    # print('delta_w: ' + str(delta_w))
+    delta_h = height - new_size[0]
+    # print('delta_h: ' + str(delta_h))
+    top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+    left, right = delta_w // 2, delta_w - (delta_w // 2)
+
+    new_im = cv.copyMakeBorder(im, top, bottom, left, right, cv.BORDER_REPLICATE)
+    # return the resized image
+    return new_im
+
+
 class MJSynthDataset(Dataset):
 
     def __init__(self, split):
@@ -49,7 +78,8 @@ class MJSynthDataset(Dataset):
         img_path = line.split(' ')[0]
         img_path = os.path.join(IMG_FOLDER, img_path)
         img = cv.imread(img_path)
-        img = cv.resize(img, (imgW, imgH), cv.INTER_CUBIC)
+        img = image_resize(img, width=imgW, height=imgH, inter=cv.INTER_CUBIC)
+        # img = cv.resize(img, (imgW, imgH), cv.INTER_CUBIC)
         img = img[..., ::-1]  # RGB
         img = transforms.ToPILImage()(img)
         img = self.transformer(img)
